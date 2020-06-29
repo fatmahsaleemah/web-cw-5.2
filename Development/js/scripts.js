27967ffc81410a58470aad4af64ff6d5061f6e63
fatmahsaleemah <66628606+fatmahsaleemah@users.noreply.github.com>
2020-06-29 21:16:22 +0300

    تم الحل 👩🏻‍💻

---
+++ Development/js/scripts.js
@@ -0,0 +1,82 @@
+
+
+
+let food2 = prompt("استخدم اسم وجبتك");
+switch(food){
+    case "بيتزا":
+    console.log("بيتزا");
+break;
+    case "بطاط":
+    console.log("بطاط");
+break;
+     case "باستا":
+    console.log("باستا");
+        break;
+     case "برجر":
+     console.log("برجر");
+break;
+default:
+console.log("طلبك غير متوفر");
+       
+
+
+
+
+
+var first_number = prompt("الرقم الاول", "");
+
+var second_number = prompt("الرقم الثاني", "");
+
+var answer = first_number * second_number;
+
+document.write(" الجواب " + answer);
+
+if (first_number == string){
+alert("اكتب ارقام ")
+}
+if (second_number == string){
+    alert("اكتب ارقام ")
+}
+
+
+
+
+
+
+let color = prompt("ما هو لونك المفضل");
+
+
+switch(color){
+case "احمر":
+alert("لون الدماء");
+break;
+case "اسود":
+alert("ملك الالوان");
+break;
+case "الابيض":
+ alert("لون الصفاء");
+    break;
+    default:
+        alert("لا يوجد لرنك المفضل اسفه")
+
+}
+
+
+
+let age = 18
+if (age == 18){
+console.log("مدرسه")
+}
+else if (age < 18){
+console.log("روضه")
+}
+else if (age > 18){
+    console.log("جامعه")
+}
+
+
+
+
+
+
+
